bcfda1cdffce37392344cc0c29386f7840e014a6
imranme <tusharimran914076@gmail.com>
2025-01-04 23:24:27 +0600

    python recupe

---
+++ python/variables_datatype.py
@@ -0,0 +1,24 @@
+# variables :
+
+# A variable is a name given to a memory location in a program
+
+name = "Tushar"
+age = 24
+price = 30.99
+
+print(name)
+print(type(name))
+
+# data type of python:
+    # Integers
+    # String  
+    # Float
+    # Boolean
+    # None
+
+# print sum:
+
+a = 2
+b = 8
+sum = a+b
+print(sum)
