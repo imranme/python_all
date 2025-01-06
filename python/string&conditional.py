259b0a75c0cec555baa5de99849c0960afdc6ecd
imranme <tusharimran914076@gmail.com>
2025-01-06 21:49:26 +0600

    python recupe

---
+++ python/string&conditional.py
@@ -0,0 +1,29 @@
+str1 = 'THis is a string. \n we are creating python'
+print(str1)
+
+# srings: string is data type that sorted a sequence of characters.
+
+# *concatenation
+# "hello" + "world" -> "helloworld"
+
+# length of str 
+
+srt1 = "taskim"
+len1 = len(str1)
+print(len1)
+
+str2 = "college"
+len2 = len(str2)
+print(len2)
+
+# if year % 4 == 0:
+#     if year % 100 == 0: 
+#         if year % 400 == 0:
+#             leap = True
+#         else:
+#             leap = False
+#     else:
+#         leap = True
+
+# return leap  hekkar rank
+
