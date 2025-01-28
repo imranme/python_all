59ec10b0150828fd47801ec22e54d80fee79845f
imranme <tusharimran914076@gmail.com>
2025-01-28 22:16:15 +0600

    MIU pytho first class

---
+++ MIU python/class_1.py
@@ -0,0 +1,25 @@
+# n1 = 5
+# n2 = 6
+# result = n1+ n2
+# print (result)
+
+# n1 = int(input("Enter a Number: "))  # Convert input to integer
+# n2 = int(input("Enter another Number: "))  
+
+# result = n1 + n2
+# print("The result of the addition is:", result)
+
+# if n1 < n2:
+#     print(n1,"That's a small number!")
+# else:
+#     print(n2,"That's a small number.")
+
+n1 = int(input("Enter a Number: "))  
+n2 = int(input("Enter another Number: ")) 
+
+
+if n1 % 2 == 0 and n2 % 2 == 0:
+    result = n1 + n2
+    print("Both numbers are even. The sum is:", result)
+else:
+    print("At least one of the numbers is not even. Cannot add them.")
