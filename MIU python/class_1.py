59cd6823e1cfe72edfbdcbe2cff16ed26390285f
imranme <tusharimran914076@gmail.com>
2025-01-28 22:53:54 +0600

    MIU pytho first class

--- MIU python/class_1.py
+++ MIU python/class_1.py
@@ -14,10 +14,6 @@
 # else:
 #     print(n2,"That's a small number.")
 
-n1 = int(input("Enter a Number 1: "))  
-n2 = int(input("Enter a number 2: ")) 
-
-
-
-result = n1 + n2 
-print(n1,"+", n2, "=", result)
+n1 = int(input("Enter a ID Number : ")) 
+if n1 < 50: 
+    print("Hello To", n1)
