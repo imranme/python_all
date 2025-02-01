1bab670afd2d17fba271d925b8e727aafe021af6
imranme <tusharimran914076@gmail.com>
2025-02-01 16:35:37 +0600

    today learn function and data type

---
+++ python3.8/class1.py
@@ -0,0 +1,51 @@
+# username = input("Your name: ")
+
+# print(f"hello {username}!")
+
+# print("hello world") #first code python 3.8
+
+        #Function/
+
+# print("1")
+
+# def complicated_logic(first, second):
+#     print(f"You passed: {first}, {second}")
+
+# number_1 = 10
+# number_2 = 3
+
+# print("2")
+# complicated_logic(number_1, number_2)
+# complicated_logic(5, 4)
+# print("3")
+
+    # Data type 
+
+number_value = 32
+second_value = 4
+
+addition = number_value + second_value
+subtraction = number_value - second_value
+mul = number_value* 2
+div = number_value / 5
+div_int = number_value // 2
+remainder = number_value % 5
+
+print(f"div: {div_int}")
+print(f"rem: {remainder}")
+
+print(f"Addtion: {addition}")
+print(f"Sub: {subtraction}")
+
+person_name = "Zulkarnine Mahmud"
+food = "burger"
+value2 = 99
+
+name_value = person_name + " " + str(value2)
+concatenated_strig = person_name + food
+print(concatenated_strig)
+print(name_value)
+print(f"Legth of name: {len(name_value)}")
+
+
+
