ce31b61e4ebb01a941d7e355ec7cf70e5a9eaa2f
imranme <tusharimran914076@gmail.com>
2025-02-02 07:16:16 +0600

    today learn list of python

--- python3.8/class1.py
+++ python3.8/class1.py
@@ -21,31 +21,52 @@
 
     # Data type 
 
-number_value = 32
-second_value = 4
+# number_value = 32
+# second_value = 4
 
-addition = number_value + second_value
-subtraction = number_value - second_value
-mul = number_value* 2
-div = number_value / 5
-div_int = number_value // 2
-remainder = number_value % 5
+# addition = number_value + second_value
+# subtraction = number_value - second_value
+# mul = number_value* 2
+# div = number_value / 5
+# div_int = number_value // 2
+# remainder = number_value % 5
 
-print(f"div: {div_int}")
-print(f"rem: {remainder}")
+# print(f"div: {div_int}")
+# print(f"rem: {remainder}")
 
-print(f"Addtion: {addition}")
-print(f"Sub: {subtraction}")
+# print(f"Addtion: {addition}")
+# print(f"Sub: {subtraction}")
 
-person_name = "Zulkarnine Mahmud"
-food = "burger"
-value2 = 99
+# person_name = "Zulkarnine Mahmud"
+# food = "burger"
+# value2 = 99
 
-name_value = person_name + " " + str(value2)
-concatenated_strig = person_name + food
-print(concatenated_strig)
-print(name_value)
-print(f"Legth of name: {len(name_value)}")
+# name_value = person_name + " " + str(value2)
+# concatenated_strig = person_name + food
+# print(concatenated_strig)
+# print(name_value)
+# print(f"Legth of name: {len(name_value)}")
 
 
+        # list 
+
+grocery_list = ["rice", "potato", "tomato"]
+l2 = list()
+secod_item = grocery_list[1]
+print(grocery_list)
+print(secod_item)
+grocery_list.append("water")
+
+print(grocery_list)
+print(l2)
+l2.append(3)
+print(l2)
+l2.append("computer")
+print(l2)
+print(grocery_list[-1]) # it's a chek last umber of list python, (-1 = 3) is a same index number
+
+
+number_list = [1,90,34,23,77,21,12,67,89,44,50] 
+number_list.sort() # sort the list
+print(number_list)
 
