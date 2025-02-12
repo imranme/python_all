2315f35e3f08cbda3817a94a9c4717ad8bb9dcfd
imranme <tusharimran914076@gmail.com>
2025-02-12 22:04:46 +0600

    python miu for loop

---
+++ MIU python/class_2.py
@@ -0,0 +1,45 @@
+# #write a python program to red three
+
+# x = int(input("Enter a number  1: "))
+# y = int(input("Enter a number  2: "))
+# z = int(input("Enter a number  3: "))
+# a = 355
+# b = 566
+
+# lgt = x
+# if y > lgt:
+#     lgt = y
+# if z > lgt:
+#     lgt = z
+# if a > lgt:
+#     lgt = a
+# if b > lgt:
+#     lgt = b
+
+# print(lgt)
+
+# tottal sum
+
+# # Read three numbers from the user
+# x = int(input("Enter a number 1: "))
+# y = int(input("Enter a number 2: "))
+# z = int(input("Enter a number 3: "))
+
+# # Predefined numbers
+# a = 10
+# b = 20
+
+# # Calculate the sum of all numbers
+# total_sum = x + y + z + a + b
+
+# # Display the sum
+# print("The sum of all numbers is:", total_sum)
+
+n = int(input("Enter a number: "))  
+total_sum = 0  
+for i in range(1, n, 2):
+    total_sum += i  
+
+print("The sum of numbers from 1 to", n, "is:", total_sum)
+
+
