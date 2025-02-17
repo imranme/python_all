6d191cf9b8d1dd860112f111433be2fbd9389384
imranme <tusharimran914076@gmail.com>
2025-02-17 23:14:48 +0600

    python oop class

--- python3.8/oparatting_overloding.py
+++ python3.8/oparatting_overloding.py
@@ -55,3 +55,17 @@ a += MyInt(3)
 
 print(a)
 
+
+## Definition
+class Add:
+## Initialization
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    def add(self):
+        return self.a+self.b
+obj = Add(3,4)
+## Access
+print obj.add()
+## Garbage collection
+
