32557749fcf30a2c531ab817c1c5a521fd4134d1
imranme <tusharimran914076@gmail.com>
2025-04-29 22:36:20 +0600

    variable day 2 class

---
+++ study marth python/day-02.py
@@ -0,0 +1,27 @@
+print('hello world', 'welcome to 60 days of python', sep='&', end='|')
+
+var = "hello world"
+print(var)
+print('My Sentence is ' +var)
+
+name = 'Tushar'
+print(name)
+
+
+#  locals vs global variable
+
+x = 2000 #global
+
+def func1():
+    x = 500 #local
+    print('My num is: ', x)
+func1()
+print('My num is: ',x)
+
+
+def func1():
+    global x
+    x = 500 #local
+    print('My num is: ', x)
+func1()
+print('My num is: ',x)
