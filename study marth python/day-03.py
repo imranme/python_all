f8a3b105681c44c9859ddccc253079cec5df09d8
imranme <tusharimran914076@gmail.com>
2025-05-13 18:28:52 +0600

    day 3 python input or string methods

--- study marth python/day-03.py
+++ study marth python/day-03.py
@@ -1,4 +1,19 @@
-x = int(input())
-y = int(input())
+# x = int(input())
+# y = int(input())
 
-print(x + y)
+# print(x + y)
+
+data = 'data sciece fun'
+print(type(data))  # This will print the type of the variable 'data'
+print(data)        # This will print the value of 'data'
+
+
+data = input()
+print(data)
+print(type(data))
+
+text = 'who dil kahan se laaun'
+print(type(text))
+print(len(text))
+import sys
+print(sys.getsizeof(text))
