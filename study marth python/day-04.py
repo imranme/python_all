f3f06c5b598f17b82a3404c7ca0211176799dbcc
imranme <tusharimran914076@gmail.com>
2025-05-16 23:34:32 +0600

    day 3 python input or string methods

---
+++ study marth python/day-04.py
@@ -0,0 +1,9 @@
+>>> "We love python!"
+'We love python!'
+>>> 'The most popular general purpose programming language'
+'The most popular general purpose programming language'
+
+
+>>> input("Give me your country name: ")
+Give me your country name: Bangladesh
+'Bangladesh'
