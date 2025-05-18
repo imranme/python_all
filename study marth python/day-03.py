7d9baeb7aa027c980c9f674b13d8adc09df6143f
imranme <tusharimran914076@gmail.com>
2025-05-18 23:44:22 +0600

    day 3 python input or string methods

--- study marth python/day-03.py
+++ study marth python/day-03.py
@@ -3,17 +3,30 @@
 
 # print(x + y)
 
-data = 'data sciece fun'
-print(type(data))  # This will print the type of the variable 'data'
-print(data)        # This will print the value of 'data'
+# data = 'data sciece fun'
+# print(type(data))  # This will print the type of the variable 'data'
+# print(data)        # This will print the value of 'data'
 
 
-data = input()
-print(data)
-print(type(data))
+# data = input()
+# print(data)
+# print(type(data))
 
-text = 'who dil kahan se laaun'
-print(type(text))
-print(len(text))
-import sys
-print(sys.getsizeof(text))
+# text = 'who dil kahan se laaun'
+# print(type(text))
+# print(len(text))
+# import sys
+# print(sys.getsizeof(text))
+
+    #   FIND FUNCTION
+data = '60 Days of python'
+print (data.find('of'))
+print (data.find('f'))
+print (data.find('y', 6,10))
+print (data.find('y', 6,13))
+
+    # INDEX FUNCTION 
+# print (data.index('ai'))
+print(data.lower())
+print(data.upper())
+
