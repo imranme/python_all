be35c8b906e8974bae7c7be1cca9f820143afb7e
imranme <tusharimran914076@gmail.com>
2025-05-19 23:20:30 +0600

    day 4 numeric datatypes

--- study marth python/day-04.py
+++ study marth python/day-04.py
@@ -1,9 +1,20 @@
->>> "We love python!"
-'We love python!'
->>> 'The most popular general purpose programming language'
-'The most popular general purpose programming language'
+# >>> "We love python!"
+# 'We love python!'
+# >>> 'The most popular general purpose programming language'
+# 'The most popular general purpose programming language'
 
 
->>> input("Give me your country name: ")
-Give me your country name: Bangladesh
-'Bangladesh'
+# >>> input("Give me your country name: ")
+# Give me your country name: Bangladesh
+# 'Bangladesh'
+
+
+
+All About Data Types in Python 
+
+Numeric Data 
+Boolean Data
+Sequece Data 
+Set Data 
+Mapping Data 
+Data Frame 
