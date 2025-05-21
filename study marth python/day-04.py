c35d23d2066ef9d28bc5745cf20c1f39871c73ad
imranme <tusharimran914076@gmail.com>
2025-05-21 15:05:24 +0600

    day 4 all about Data Types in python

--- study marth python/day-04.py
+++ study marth python/day-04.py
@@ -10,11 +10,37 @@
 
 
 
-All About Data Types in Python 
+# All About Data Types in Python 
 
-Numeric Data 
-Boolean Data
-Sequece Data 
-Set Data 
-Mapping Data 
-Data Frame 
+# Numeric Data 
+# Boolean Data
+# Sequece Data 
+# Set Data 
+# Mapping Data 
+# Data Frame 
+
+
+# all about data types in python
+    # Numeric Data --> 1. int, 2.float, 3.complex
+
+
+x  = 10  # int
+y  =  20.4 # float
+z  =   10+6j # 1o is real number and 6j is integer
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+print(isinstance(z,complex)) 
+print(isinstance(11,complex)) 
+
+n = complex(input('Enter your complex number:- '))
+print(n)
+
+        # Boll 
+
+x = True
+y = False
+
+print(x)
