47ddb3c9bddb6db7e23d09de94b319d4f658d772
imranme <tusharimran914076@gmail.com>
2025-07-28 13:36:21 +0600

    python conditions

---
+++ Innovative_skills_python/conditional.py
@@ -0,0 +1,16 @@
+name = 'rr'
+phone = '4234325435'
+size_name = len(name)
+size_phone = len(phone)
+
+if(name=='' or phone==''):
+    print("The name field can not be empty")
+else:
+    #Nested conditions statment 
+    if(size_name<3):
+        print("The name length must be minimam 3")
+    elif(size_phone!=11):
+        print("The phome number must be 11 digit")
+
+    else:
+        print('success')
