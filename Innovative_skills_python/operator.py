d44a0ecc96b7cf3906b8038d71de10e764ced6ba
imranme <tusharimran914076@gmail.com>
2025-07-28 12:43:12 +0600

    operator python

---
+++ Innovative_skills_python/operator.py
@@ -0,0 +1,11 @@
+beef_meat_price = 8000
+chicken_meat_price = 200
+
+quantity_beef_meat = 5
+quantity_chicken_meat = 10
+
+total_price_beef_meat = beef_meat_price * quantity_beef_meat
+total_price_chicken_meat = chicken_meat_price * quantity_chicken_meat
+
+print("The total price of beef meat: ", total_price_beef_meat)
+print("The total price of Chicken meat: ", total_price_chicken_meat)
