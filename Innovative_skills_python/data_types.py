a9d2cb85610f4cd680f41bdbf81c1f3e35929aee
imranme <tusharimran914076@gmail.com>
2025-07-28 12:28:06 +0600

    data type recup

--- Innovative_skills_python/data_types.py
+++ Innovative_skills_python/data_types.py
@@ -3,3 +3,11 @@ product_name = 'beef'
 
 type_product_name = type(product_name)
 print(type_product_name)
+
+datatype = 12
+manarat = "univercity"
+
+manarattype = type(manarat)
+taypedata = type(datatype)
+# print(datatype)
+print(manarat)
--- Innovative_skills_python/loop.py
+++ Innovative_skills_python/loop.py
@@ -9,3 +9,13 @@ given_range = 10
 for i in range(given_range):
     if i%2 == 0:
         print(i)
+
+# Example 3: Python program to calculate the sum of all numbers from 1 to a given number.
+
+given_number = 10
+
+sum = 0
+
+for i in range(1,given_number+1):
+    sum+=i
+print(sum)
