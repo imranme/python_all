eac1a782001f964673eb571036a4eb0d6f00823a
imranme <tusharimran914076@gmail.com>
2025-07-31 03:07:29 +0600

    1D list part

---
+++ Innovative_skills_python/list.py
@@ -0,0 +1,7 @@
+product_name = ["chicken", "egg", "salt"]
+size_product_name_list = len(product_name)
+print(size_product_name_list)
+print(product_name[size_product_name_list-1])
+
+for i in range(size_product_name_list):
+    print(product_name[i])
