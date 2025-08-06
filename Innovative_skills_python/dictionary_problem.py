80b92d082881a743a5a51fc0135050b294b79ca0
imranme <tusharimran914076@gmail.com>
2025-08-06 23:40:26 +0600

    dictonary problem

---
+++ Innovative_skills_python/dictionary_problem.py
@@ -0,0 +1,10 @@
+product= {
+    'naem':['p1', 'p2','p3','p4'],
+    'price': [100, 200, 300, 400]
+}
+
+key_list = list(product())
+
+for i in range(len(product[key_list[0]])):
+    for j in range(len(key_list)):
+
