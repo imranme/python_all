fcd16fb26c2289093e99218bc928fd09c2ca3497
imranme <tusharimran914076@gmail.com>
2025-08-07 05:56:18 +0600

    dictonary problem done

--- Innovative_skills_python/dictionary_problem.py
+++ Innovative_skills_python/dictionary_problem.py
@@ -3,8 +3,11 @@ product= {
     'price': [100, 200, 300, 400]
 }
 
-key_list = list(product())
+key_list = list(product) 
 
 for i in range(len(product[key_list[0]])):
     for j in range(len(key_list)):
+        colum = key_list[j] 
+        print(product[colum][i], end= ' ')
 
+    print()
