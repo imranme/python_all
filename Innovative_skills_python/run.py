7ba4c8371c159c16517e6c8530093a26d4dfcdf0
imranme <tusharimran914076@gmail.com>
2025-08-11 06:39:01 +0600

    python data structure list tuple set and dict

--- Innovative_skills_python/run.py
+++ Innovative_skills_python/run.py
@@ -1,4 +1,13 @@
-user_input = " "
-while user_input != "stop":
-    user_input = input("Enter sommething(type 'sotp' to exit): ")
-    print("You entered:", user_input)
+# user_input = " "
+# while user_input != "stop":
+#     user_input = input("Enter sommething(type 'sotp' to exit): ")
+#     print("You entered:", user_input)
+
+
+unique_numbers = {1, 2, 3, 4, 4}
+print(unique_numbers)   # আউটপুট: {1, 2, 3, 4}
+unique_numbers.add(5)   # নতুন আইটেম যোগ করা
+print(unique_numbers)   # আউটপুট: {1, 2, 3, 4, 5}
+unique_numbers.add(6)
+print(unique_numbers)
+
