322b0e5e844d6257c31410cad725ecd3c9799a18
imranme <tusharimran914076@gmail.com>
2025-08-21 11:01:00 +0600

    python class & instance attribute

--- Innovative_skills_python/apna_class.py
+++ Innovative_skills_python/apna_class.py
@@ -1,40 +1,60 @@
-# Class & Object in python
+# # Class & Object in python
 
-# class is a blueprint for creating objects
+# # class is a blueprint for creating objects
 
-# creating class
+# # creating class
+
+# class Student:
+#     name = "Tushar Khan"
+
+
+# #creating object(instance)
+
+# s1 = Student()
+# print(s1.name)
+
+# s2 = Student()
+# print(s2.name)
+
+# #  creating car class 
+
+# class car:
+#     color = "blue"
+#     brand = "mercedes"
+
+#     # creating a car object
+# car1 = car()
+# print(car.color)
+# print(car.brand)
+
+
+# # creating class 
+
+# class Student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+
+# #  creating object
+
+# s1 = Student("Tushar", 24)
+# print(s1.name , s1.age)
+
+# s2 = Student("Sabbir", 22)
+# print(s2.age, s2.name)
+
+    # class & instance Attributes 
+
+# class Attributes
+# instance Attribute
 
 class Student:
-    name = "Tushar Khan"
-
-
-#creating object(instance)
-
-s1 = Student()
-print(s1.name)
-
-s2 = Student()
-print(s2.name)
-
-#  creating car class 
-
-class car:
-    color = "blue"
-    brand = "mercedes"
-
-    # creating a car object
-car1 = car()
-print(car.color)
-print(car.brand)
-
-
-# creating class 
-
-class Student:
+    univercity_name = "Manarat International Univercity" # class Attributes
     def __init__(self, name, age):
-        self.name = name
+        self.name = name # instance Attributes
         self.age = age
-
+        print("affing new student in Database..")
 
 #  creating object
 
@@ -43,3 +63,10 @@ print(s1.name , s1.age)
 
 s2 = Student("Sabbir", 22)
 print(s2.age, s2.name)
+
+s3 = Student("Nasim", 21)
+print(s3.age, s3.name)
+
+print(s2.univercity_name)
+print(s1.univercity_name)
+print("Nasim Uni name",s3.univercity_name)
