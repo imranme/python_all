37e24f3f1ba27de838874eb7cfcc51711d8b3240
imranme <tusharimran914076@gmail.com>
2025-08-21 11:18:56 +0600

    python method

--- Innovative_skills_python/apna_class.py
+++ Innovative_skills_python/apna_class.py
@@ -49,24 +49,44 @@
 # class Attributes
 # instance Attribute
 
+# class Student:
+#     univercity_name = "Manarat International Univercity" # class Attributes eta memory te ak bar store hoy
+#     # name = "anonymous" # class attr
+#     def __init__(self, name, age):
+#         self.name = name #(obj attr> class attr , same thakle obj age print hobe)# instance Attributes ektar jonno ek ek bar store hoy
+#         self.age = age
+#         print("affing new student in Database..")
+
+# #  creating object
+
+# s1 = Student("Tushar", 24)
+# print(s1.name , s1.age)
+
+# s2 = Student("Sabbir", 22)
+# print(s2.age, s2.name)
+
+# s3 = Student("Nasim", 21)
+# print(s3.age, s3.name)
+
+# print(s2.univercity_name)
+# print(s1.univercity_name)
+# print("Nasim Uni name",s3.univercity_name)
+
+    # Methods
+
 class Student:
-    univercity_name = "Manarat International Univercity" # class Attributes
+    univercity_name = "Manarat International Univercity" 
+
     def __init__(self, name, age):
-        self.name = name # instance Attributes
+        self.name = name 
         self.age = age
-        print("affing new student in Database..")
 
-#  creating object
-
+    def welcome(self):
+        print("welcome stident,",self.name)
+    
+    def get_age(self):
+        return self.age  
+      
 s1 = Student("Tushar", 24)
-print(s1.name , s1.age)
-
-s2 = Student("Sabbir", 22)
-print(s2.age, s2.name)
-
-s3 = Student("Nasim", 21)
-print(s3.age, s3.name)
-
-print(s2.univercity_name)
-print(s1.univercity_name)
-print("Nasim Uni name",s3.univercity_name)
+s1.welcome()
+print(s1.get_age())
