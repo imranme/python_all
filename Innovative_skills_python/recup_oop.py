12ade3ec8aab7a0b7887ab2c19d79b9e81b23763
imranme <tusharimran914076@gmail.com>
2025-09-04 23:39:50 +0600

    class and obj

--- Innovative_skills_python/recup_oop.py
+++ Innovative_skills_python/recup_oop.py
@@ -25,3 +25,22 @@ class Calculator:
 calc = Calculator()
 
 print("Brand: ", calc.brand)
+
+class Phone:
+    manufactures = 'china'
+
+    def __init__(self,owner,brand,price):
+        self.owner = owner
+        self.brand = brand
+        self.price = price
+    
+    def send_sms(self, phone, sms):
+        text = f'sending to: {phone} {sms}'
+        print(text)
+
+my_phone = Phone('khan', 'sumsung', 25999)
+print(my_phone.owner, my_phone.brand, my_phone.price)
+
+
+her_phone = Phone('she', 'iphpone', 32423493)
+print(her_phone.owner)
