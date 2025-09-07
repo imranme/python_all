e0dff17336545054276e678293d2a9b054053632
imranme <tusharimran914076@gmail.com>
2025-09-07 23:21:01 +0600

    studenr class

---
+++ Innovative_skills_python/Student.py
@@ -0,0 +1,25 @@
+# ২. স্টুডেন্ট ও কোর্স
+
+# একটি Student ক্লাস বানাও, যেখানে নাম, রোল নম্বর এবং কোন কোন কোর্সে ভর্তি হয়েছে সেই তথ্য থাকবে।
+# এতে মেথড থাকবে:
+
+# নতুন কোর্সে ভর্তি হওয়া (enroll in course)
+
+# সব ভর্তি করা কোর্স দেখা (view courses)
+
+class Student:
+    def __init__(self, name, roll):
+        self.name = name
+        self.roll = roll
+        self.courses = []
+
+    def enroll(self, course):
+        self.courses.append(course)
+        
+    def view_couress(self):
+        return self.courses
+    
+s1 = Student("tushar", 101)
+s1.enroll("Python")
+s1.enroll("OOP")
+print("tushar courses:", s1.view_couress)
