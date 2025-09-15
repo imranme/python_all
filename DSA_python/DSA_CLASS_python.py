996cb81448fab74be903ea7f142f7ce396f609a4
imranme <tusharimran914076@gmail.com>
2025-09-15 14:08:33 +0600

    class problem function

--- DSA_python/DSA_CLASS_python.py
+++ DSA_python/DSA_CLASS_python.py
@@ -18,3 +18,30 @@ mydog = Dog('Bagha', 2, 'brown')
 mydog.bark()
 
 print(mydog.get_age())
+
+class Person:
+    # """
+    # একটি Person অবজেক্টের মধ্যে থাকবে:
+    #   - first_name
+    #   - last_name
+    #   - gender
+    #   - address
+    # এবং describe_user() মেথড সেই ব্যক্তির নাম ও ঠিকানা দেখাবে।
+
+    # কনস্ট্রাক্টর: অবজেক্ট তৈরি হলে এট্রিবিউট সেট করবে
+    def __init__(self, first_name, last_name, gender, address):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.gender = gender
+        self.address = address
+
+    # ব্যবহারকারীর তথ্য প্রিন্ট করবে
+    def describe_user(self):
+        print(f"{self.first_name} {self.last_name} lives in {self.address}")
+
+
+# অবজেক্ট তৈরি
+person1 = Person("Alamgir", "Hossain", "male", "Ramna Thana")
+
+# মেথড কল
+person1.describe_user()
